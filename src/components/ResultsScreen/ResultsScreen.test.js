01564ba80ddac824ca845169cfdf0bf4aa398bc7
iryna babich <irene.maxweb@gmail.com>
2019-02-21 19:43:28 -0700

    Tests for the game duration in singular/plural form. Resolved #9

--- src/components/ResultsScreen/ResultsScreen.test.js
+++ src/components/ResultsScreen/ResultsScreen.test.js
@@ -19,3 +19,18 @@ it("renders correct totalWrongGuessesCount with wrong guesses > 0", () => {
   );
   expect(wrapper.find(".is-wrong-count").text()).toBe(`${wrongGuesses}`);
 });
+
+it("renders no minutes when duration of the game less than a minute", () => {
+  const wrapper = mount(<ResultsScreen duration={10} />);
+  expect(wrapper.find(".duration-minutes").length).toBe(0);
+});
+
+it("renders minutes in singular form when duration of the game one minute", () => {
+  const wrapper = mount(<ResultsScreen duration={70} />);
+  expect(wrapper.find(".duration-minutes").text()).toBe("1 minute");
+});
+
+it("renders minutes in plural form when duration of the game more than one minute", () => {
+  const wrapper = mount(<ResultsScreen duration={130} />);
+  expect(wrapper.find(".duration-minutes").text()).toBe("2 minutes");
+});
